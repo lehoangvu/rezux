d6582a3f9ed3a5561c71542bc565e2e66c4afa3f
lehoangvu <hoangvu171819@gmail.com>
2017-03-29 01:49:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,12 @@ app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname) //where your static content is located in your filesystem
 );
+app.route('/:playlistId').get(function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+app.route('/:playlistId/:songId').get(function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
 
 app.listen(process.env.PORT || 5000); //the port you want to use

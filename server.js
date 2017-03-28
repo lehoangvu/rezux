da803fadbde8652325a8b2e4c92f08bc7e149eff
lehoangvu <hoangvu171819@gmail.com>
2017-03-29 01:17:38 +0700

    Update server.js
    update server with port

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-// if (typeof localStorage === "undefined" || localStorage === null) {
-//   var LocalStorage = require('node-localstorage').LocalStorage;
-//   localStorage = new LocalStorage('./scratch');
-// }
-
 var express = require('express');
 var app = express();
-// app.get("/:playlistId/:songId.html", function (req, res) {
-//   express.static(__dirname)
-// });
-
 app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname) //where your static content is located in your filesystem
 );
 
-app.route('/:playlistId').get(function (req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
-
-app.route('/:playlistId/:songId').get(function (req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
-
-app.listen(3000); //the port you want to use
+app.listen(process.env.PORT || 5000); //the port you want to use

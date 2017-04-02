b3990d91770078e8807a8653bfaedcdb57ea78c7
lehoangvu <hoangvu171819@gmail.com>
2017-04-02 16:07:49 +0700

    fix http

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
-function requireHTTPS(req, res, next) {
-    if (!req.get('x-arr-ssl') && process.env.APP_ENV && process.env.APP_ENV === 'production') {
-        return res.redirect('https://' +req.get('host') + req.url);
-    }
-    next();
-}
- 
-app.use(requireHTTPS);
+
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https' && process.env.APP_ENV && process.env.APP_ENV === 'production')
+    res.redirect('https://' +req.get('host') + req.url);
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
 app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname) //where your static content is located in your filesystem

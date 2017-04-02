156ed0a514dff6a702478ba8dd9b2660a7c4bdf7
lehoangvu <hoangvu171819@gmail.com>
2017-04-02 15:47:48 +0700

    fix style and https

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+var http       = require('http');
 var express = require('express');
 var app = express();
+
+// set up a route to redirect http to https
+app.all('*',function(req,res, next){  
+    if (/^http$/.test(req.protocol) && process.env.APP_ENV && process.env.APP_ENV === 'production') {
+        return res.redirect('https://' + req.headers.host + req.url)
+    } else {
+        return next();
+    }
+});
+
 app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname) //where your static content is located in your filesystem
@@ -16,4 +27,7 @@ app.route('/auth').get(function (req, res) {
 // });
 
 
-app.listen(process.env.PORT || 5000); //the port you want to use
+// app.listen(process.env.PORT || 5000); //the port you want to use
+http.createServer(app).listen(process.env.PORT || 5000).on('listening', function() {
+  return console.log("HTTP to HTTPS redirect app launched.");
+});
